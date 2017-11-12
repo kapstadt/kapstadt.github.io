5fb32e7b5f862a7cc92b598c4e829a5cf01443f3
t1nak <tinakoziol@yahoo.de>
2017-11-12 18:37:09 +0200

    build

---
+++ js/agency.js
@@ -0,0 +1,36 @@
+// Smooth scrolling via animate()
+$(document).ready(function(){
+  if ($("body").attr("home")) {
+    $(".anchor").on('click', function(event) {
+      if (this.hash) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  }
+});
+
+$(document).ready(function(){
+  var maxOffset = 300;
+  if ($(window).scrollTop() >= maxOffset) {
+    $('.navbar-default').addClass('navbar-shrink');
+  }
+  else {
+    $('.navbar-default').removeClass('navbar-shrink');
+  }
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
